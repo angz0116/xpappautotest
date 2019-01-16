0be7ba9f70002eabe1a1d858df69f136edf5d660
zhaoai <aizhao@jieyuechina.com>
2019-01-16 11:30:54 +0800

    新增公用配置信息

---
+++ config/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+"""
+@project = xpappautotest
+@file = __init__.py
+@author = zhaoai
+@create_time = 2019-1-16 11:04
+"""
---
+++ config/config.py
@@ -0,0 +1,65 @@
+# -*- coding:utf-8 -*-
+"""
+@project = xpappautotest
+@file = config
+@author = zhaoai
+@create_time = 2019-1-16 11:08
+"""
+DB_CONF_CORE3 = {
+	"host": "172.18.100.126",
+	"port": 1521,
+	"username": "core0915",
+	"pwd": "core0915",
+	"dbservice": "testdb"
+}
+
+DB_CONF_LC3 = {
+	"host": "172.18.100.126",
+	"port": 1521,
+	"username": "lc0915",
+	"pwd": "lc0915",
+	"dbservice": "testdb"
+}
+APPIUM_HOST = "192.168.75.21"
+APPIUM_PORT = 4723
+APPIUM_CONN = "62001"
+
+ANDROID5 = {
+	"host": APPIUM_HOST,
+	"port": APPIUM_PORT,
+	"platform": "Andriod",
+	"version": "5.1.1",
+	"conn": APPIUM_HOST+":"+APPIUM_CONN
+}
+ANDROID4 = {
+	"host": APPIUM_HOST,
+	"port": APPIUM_PORT,
+	"platform": "Andriod",
+	"version": "4.2.2",
+	"conn": APPIUM_HOST+":"+APPIUM_CONN
+}
+
+ANDROID7 = {
+	"host": APPIUM_HOST,
+	"port": APPIUM_PORT,
+	"platform": "Andriod",
+	"version": "7.0.0",
+	"conn": APPIUM_HOST+":"+APPIUM_CONN
+}
+
+ANDROID8 = {
+	"host": APPIUM_HOST,
+	"port": APPIUM_PORT,
+	"platform": "Andriod",
+	"version": "8.0.0",
+	"conn": APPIUM_HOST+":"+APPIUM_CONN
+}
+
+ANDROID71 = {
+	"host": APPIUM_HOST,
+	"port": APPIUM_PORT,
+	"platform": "Andriod",
+	"version": "7.0.0",
+	"conn": APPIUM_HOST+":"+APPIUM_CONN
+}
+
